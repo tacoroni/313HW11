753ebb8abbe4c103c1759fdc6270c631afe4a89b
queso <c.ayala0527@gmail.com>
2025-05-13 17:24:53 -0400

    backup

--- translate2Ascii.asm
+++ translate2Ascii.asm
@@ -3,8 +3,8 @@
 section .data
 inputbuf:
     db 0x83,0x6A,0x88,0xDE,0x9A,0xC3,0x54,0x9A ;data to be output in ASCII format
-length:
-    equ 8
+asciistuff:
+    db '0123456789ABCDEF'
 
 section .bss
 outputbuf:
@@ -16,33 +16,51 @@ global _start
 _start:
     mov edi, outputbuf
     mov esi, inputbuf
-    mov ecx, length
+    mov ecx, 8
 
-loop_and_convert:
+convert_bytes:
 
     ;converting first half, ex 0x83 -> 0x8
     ;use al so we only use 8 bits
-    mov al, [esi]
+    mov al, byte [esi]
     AND al, 0xF0
     shr al, 4
-
+    movzx eax, al
+    mov ah, [asciistuff+eax]
+    mov [edi], ah
+    inc edi
 
     ;converting second half ex 0x83 ->0x3
 
-    mov al, [esi]
+    mov al, byte [esi]
     AND al, 0x0F
+    movzx eax, al
+    mov ah, [asciistuff+eax]
+    mov [edi], ah
+    inc edi
     
-    inc esi ;get next byte and loop again
-    loop loop_and_convert
+    ; add a space and continue
+    mov byte [edi], ' '
+    inc edi
+
+    ;if still more bytes, esi != 0 so keep looping
+    inc esi 
+    dec ecx
+    jnz convert_bytes
+
+    mov byte [edi], 0X0A
+    inc edi
     
 
     ;print and exit
+    sub edi, outputbuf
     mov eax, 4
     mov ebx, 1
+    mov edx, edi
     mov ecx, outputbuf
     int 80h
 
     mov eax, 1
-    xor eax, ebx
+    mov ebx, 0
     int 80h
 
